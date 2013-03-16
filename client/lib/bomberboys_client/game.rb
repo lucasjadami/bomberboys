c7491916a478d5c2a372f8d64614f28235d63511
Rafael Regis <rafael.regisp@gmail.com>
2013-03-16 02:11:39 -0300

    Move message handler to game.

---
+++ client/lib/bomberboys_client/game.rb
@@ -0,0 +1,23 @@
+class Game
+  def initialize
+    @board = Board.new
+  end
+
+  def add_player(id, name)
+  end
+
+  def remove_player(id)
+  end
+
+  def move_player(id, x, y)
+  end
+
+  def add_bomb(id, x, y)
+  end
+
+  def explode_bomb(id)
+  end
+
+  def fall_player(id)
+  end
+end
