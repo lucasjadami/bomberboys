7b6ffad5476f622cf53fddb7236f7524e98bd9ee
Rafael Regis <rafael.regisp@gmail.com>
2013-03-17 12:57:04 -0300

    Add socket to connection.

--- client/lib/bomberboys_client/connection.rb
+++ client/lib/bomberboys_client/connection.rb
@@ -1,6 +1,7 @@
 module BomberboysClient
   class Connection 
-    def initialize(address, port)
+    def initialize(socket)
+      @socket = socket
     end
 
     def register(listener)
@@ -8,23 +9,22 @@ module BomberboysClient
     end
 
     def start
-      str_message = "h\n\n\n\n\n\nasdf"
-      loop do
+      while str_message = @socket.gets
         message = Message.unpack(str_message)
         listener.send(message.action, *message.params)
       end
     end
 
     def login(name)
-      Message.new(:login, [name]).pack
+      @socket.print Message.new(:login, [name]).pack
     end
 
     def move_me(direction)
-      Message.new(:move_me, [direction]).pack
+      @socket.print Message.new(:move_me, [direction]).pack
     end
 
     def plant_bomb
-      Message.new(:plant_bomb).pack
+      @socket.print Message.new(:plant_bomb).pack
     end
   end
 end
