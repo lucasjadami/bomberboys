8e6e4c36269f5ef8a6e6384569eda85614548e52
Rafael Regis <rafael.regisp@gmail.com>
2013-03-17 13:16:22 -0300

    Add usage of lib.

--- client/lib/bomberboys_client.rb
+++ client/lib/bomberboys_client.rb
@@ -1,4 +1,10 @@
-require "bomberboys_client/version"
+require_relative "bomberboys_client/version"
+
+require_relative "bomberboys_client/board"
+require_relative "bomberboys_client/connection"
+require_relative "bomberboys_client/game"
+require_relative "bomberboys_client/player"
+require_relative "bomberboys_client/message"
 
 module BomberboysClient
   # Your code goes here...
