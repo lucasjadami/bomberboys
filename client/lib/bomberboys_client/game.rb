55052403f10d4bad114085ae09388c9fe9005789
Rafael Regis <rafael.regisp@gmail.com>
2013-03-16 23:47:34 -0300

    Implement add_player message parsing.

--- client/lib/bomberboys_client/game.rb
+++ client/lib/bomberboys_client/game.rb
@@ -4,8 +4,14 @@ module BomberboysClient
       @board = Board.new
     end
 
-    def add_player(id, name)
-      @board.players[player.id] = Player.new(id, name, @board.players.empty?)
+    def add_player(params)
+      id   = bytes_to_str(params[0..1])
+      x    = bytes_to_str(params[2..3]).to_i
+      y    = bytes_to_str(params[4..5]).to_i
+      name = bytes_to_str(params[6..-1])
+
+      @board.players[id] = Player.new(id, name, @board.players.empty?)
+      @board.players[id].move(x, y)
     end
 
     def remove_player(id)
@@ -26,6 +32,11 @@ module BomberboysClient
 
     def explode_bomb(id)
       @board.bombs[id].explode
+    end
+
+    private
+    def bytes_to_s(bytes)
+      bytes.map { |b| b.chr }.join
     end
   end
 end
