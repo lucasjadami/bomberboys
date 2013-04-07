acc5162087c6d8c2a06f1f992543164faa66a6b0
Rafael Regis <rafael.regisp@gmail.com>
2013-04-06 22:27:43 -0300

    Encapsulate communication in ServerInterface.

--- client/lib/bomberboys_client.rb
+++ client/lib/bomberboys_client.rb
@@ -6,6 +6,7 @@ require_relative "bomberboys_client/game"
 require_relative "bomberboys_client/player"
 require_relative "bomberboys_client/message"
 require_relative "bomberboys_client/dummy_bot"
+require_relative "bomberboys_client/server_interface"
 
 module BomberboysClient
   # Your code goes here...
--- client/lib/bomberboys_client/connection.rb
+++ client/lib/bomberboys_client/connection.rb
@@ -1,7 +1,7 @@
 module BomberboysClient
   class Connection 
-    def initialize(socket)
-      @socket = socket
+    def initialize(server)
+      @server = server 
     end
 
     def register(listener)
@@ -9,8 +9,7 @@ module BomberboysClient
     end
 
     def start
-      while str_message = @socket.recv(100)
-        message = Message.unpack(str_message)
+      while message = @server.receive
         if @listener.respond_to?(message.action)
           @listener.send(message.action, *message.params)
         else
@@ -20,16 +19,16 @@ module BomberboysClient
     end
 
     def login(name)
-      @socket.print Message.new(:login, [name]).pack
+      @server.send(Message.new(:login, [name]))
     end
 
     def move_me(direction)
       dir = { n:0, ne:1, e:2, se:3, s:4, sw:5, w:6, nw:7 }
-      @socket.print Message.new(:move_me, [dir[direction]]).pack
+      @server.send(Message.new(:move_me, [dir[direction]]))
     end
 
     def plant_bomb
-      @socket.print Message.new(:plant_bomb).pack
+      @server.send(Message.new(:plant_bomb))
     end
   end
 end
