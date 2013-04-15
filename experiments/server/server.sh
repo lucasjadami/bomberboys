5830294481a473db26a92191fbae5c5a37cc1d0d
Lucas J. Adami <lucasjadami@gmail.com>
2013-04-15 14:00:37 -0300

    Modify server script

--- experiments/server/server.sh
+++ experiments/server/server.sh
@@ -1,6 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+#Params: server executable (a bin folder must be created on the same directory)
 
-for i in `seq 1 10`; do
-	./$1 > output/$ALIAS($i).server
-	cat /proc/loadavg > output/$1($i).cpu
+if [ ! -d "output" ]; then
+	mkdir output
+fi
+
+runTime=10
+
+echo "Running experiment for $1"
+
+for executionId in `seq 1 10`; do
+
+	echo "Current execution is $executionId, press RETURN (any key) to start"
+	read -n 1
+	echo "Started execution, ending in $runTime seconds"
+	
+	./bin/$1 > output/$1-$executionId.server &
+	pId=$!
+	
+	sleep $runTime
+	# Send SIGINT (2, CTRL + C) to the server to terminate properly.
+	kill -2 $pId
+	
+	cat /proc/loadavg > output/$1-$executionId.cpu
 done 
+
