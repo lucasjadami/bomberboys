bae3c5ee9cc52ed803a631889862c0f7812b1a39
Rafael Regis <rafael.regisp@gmail.com>
2013-04-27 19:35:27 -0300

    Add all script to execute all the experiments.

---
+++ experiments/server/all.sh
@@ -0,0 +1,8 @@
+./server-trash0-tcp-blocking.sh;
+./server-trash0-tcp-nonblocking.sh;
+./server-trash100-tcp-blocking.sh;
+./server-trash100-tcp-nonblocking.sh;
+./server-trash0-udp-blocking.sh;
+./server-trash0-udp-nonblocking.sh;
+./server-trash100-udp-blocking.sh;
+./server-trash100-udp-nonblocking.sh;
