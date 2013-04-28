0db1d8971e771ebf52ba4f715ed90f0ae9c6573a
Rafael Regis <rafael.regisp@gmail.com>
2013-04-28 17:56:14 -0300

    Change hosts setup.

--- experiments/client/client-trash0-udp.sh
+++ experiments/client/client-trash0-udp.sh
@@ -1,2 +1,2 @@
 export JAVA_HOME=/opt/wsarch-env-client/jdk1.7.0
-./bomberboys/experiments/client/jclient-0-trash/bin/jclient 10 udp vm2-h6 10011
+./bomberboys/experiments/client/jclient-0-trash/bin/jclient 10 udp desktop-15 10011
