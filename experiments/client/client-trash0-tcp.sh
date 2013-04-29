c4520acca5dcddd40b94646c0f19753f4ec03383
Rafael Regis <rafael.regisp@gmail.com>
2013-04-29 11:35:23 -0300

    Change server host.

--- experiments/client/client-trash0-tcp.sh
+++ experiments/client/client-trash0-tcp.sh
@@ -1,5 +1,2 @@
 export JAVA_HOME=/opt/wsarch-env-client/jdk1.7.0
-./bomberboys/experiments/client/jclient-0-trash/bin/jclient 10 tcp desktop-15 10011 &
-pId=$!
-sleep 90
-kill -2 $pId
+./bomberboys/experiments/client/jclient-0-trash/bin/jclient 10 tcp vm1-h6 10011 &
--- experiments/client/client-trash100-udp.sh
+++ experiments/client/client-trash100-udp.sh
@@ -1,5 +1,2 @@
 export JAVA_HOME=/opt/wsarch-env-client/jdk1.7.0
-./bomberboys/experiments/client/jclient-100-trash/bin/jclient 10 udp desktop-15 10011 &
-pId=$!
-sleep 90
-kill -2 $pId
+./bomberboys/experiments/client/jclient-100-trash/bin/jclient 10 udp vm1-h6 10011 &
