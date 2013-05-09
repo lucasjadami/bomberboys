bb972fc4a121c2f127259a872047757a6d90e8b8
Rafael Regis <rafael.regisp@gmail.com>
2013-05-09 10:11:56 -0300

    Remove control code for experiments and multiple bots.

--- client/src/main/java/com/bomberboys/main/Main.java
+++ client/src/main/java/com/bomberboys/main/Main.java
@@ -2,71 +2,30 @@ package com.bomberboys.main;
 
 import com.bomberboys.main.game.Game;
 
-/**
- *
- */
-public class Main
-{
-    public static int activeThreads;
-
-    public static void main(String[] args) throws Exception
-    {
-        System.out.println("Usage: n_bots [tcp|udp] address port\n");
-        int nBots = Integer.parseInt(args[0]);
-        String protocol = args[1];
-        String address = args[2];
-        int port = Integer.parseInt(args[3]);
-
-        activeThreads = nBots;
-        startGameThread(protocol, address, port, true);
-        for (int i = 1; i < nBots; ++i)
-        {
-            startGameThread(protocol, address, port, false);
-        }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Usage: address port\n");
+        startGameThread(args[0], Integer.parseInt(args[1]));
     }
 
-    private static void startGameThread(final String protocol, final String address,
-                                        final int port, final boolean informer)
-    {
-        new Thread()
-        {
+    private static void startGameThread(final String address, final int port) {
+        new Thread() {
             @Override
-            public void run()
-            {
-                Game game = new Game(protocol, address, port, informer);
+            public void run() {
+                Game game = new Game(address, port);
                 game.connect();
                 
                 long nextAck = System.currentTimeMillis() + 1000;
                 long now = 0;
                 
-                while (!game.isShutdown())
-                {
+                while (!game.isShutdown()) {
                     now = System.currentTimeMillis();
-                    try
-                    {
+                    try {
                         Thread.sleep(50);
-                    }
-                    catch (Exception ex)
-                    {
-                    }
+                    } catch (Exception ex) { }
                     
                     game.processPackets();
                     game.doRandomAction();
-
-                    if (nextAck < now)
-                    {
-                        if (game.isInformer())
-                        {
-                            game.sendPing();
-                        }
-                        game.sendAcknowledge();
-                        nextAck = now + 1000;
-                    }
-                }
-                System.out.println("Ended normally.");
-                activeThreads--;
-                if (activeThreads == 0) {
-                    System.exit(0);
                 }
             }
         }.start();
